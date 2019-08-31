4540a015fdf9582bd8ca59c0e4693e859e4e924f
boddiul <35450408+boddiul@users.noreply.github.com>
2019-08-31 19:52:25 +0300

    testing

---
+++ src/main.js
@@ -0,0 +1,23 @@
+var game = new Phaser.Game(720, 1280, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+
+
+function preload() {
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
+    game.load.image('back', 'assets/back.png');
+
+}
+
+
+
+function create() {
+
+    game.add.sprite(0, 0, 'back');
+}
+
+
+function update() {
+
+}
